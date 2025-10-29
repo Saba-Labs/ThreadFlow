a8cfd76a6852c230e72dccaab66e66dfca48c5c8
sabarish-arjunan <sabari.zapper@gmail.com>
2025-10-29 13:11:10 +0530

    Update App.tsx

--- client/App.tsx
+++ client/App.tsx
@@ -1,9 +1,9 @@
 import "./global.css";
-
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { FontSizeProvider } from "./hooks/use-font-size";
+import { SearchProvider } from "./context/SearchContext"; // ADD THIS
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Settings from "./pages/Settings";
@@ -15,30 +15,35 @@ import NewModel from "./pages/NewModel";
 import EditModel from "./pages/EditModel";
 import Roadmap from "./pages/Roadmap";
 import ReStok from "./pages/ReStok";
+
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <Routes>
-        <Route element={<AppLayout />}>
-          <Route path="/" element={<Index />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route
-            path="/settings/production-path"
-            element={<SettingsProductionPath />}
-          />
-          <Route path="/models/all" element={<ModelsAll />} />
-          <Route path="/job-work" element={<JobWork />} />
-          <Route path="/models/new" element={<NewModel />} />
-          <Route path="/models/:id/edit" element={<EditModel />} />
-          <Route path="/roadmap" element={<Roadmap />} />
-          <Route path="/restok" element={<ReStok />} />
-        </Route>
-        {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <SearchProvider> {/* ADD THIS */}
+      <FontSizeProvider> {/* WRAP FontSizeProvider if you use it */}
+        <BrowserRouter>
+          <Routes>
+            <Route element={<AppLayout />}>
+              <Route path="/" element={<Index />} />
+              <Route path="/settings" element={<Settings />} />
+              <Route
+                path="/settings/production-path"
+                element={<SettingsProductionPath />}
+              />
+              <Route path="/models/all" element={<ModelsAll />} />
+              <Route path="/job-work" element={<JobWork />} />
+              <Route path="/models/new" element={<NewModel />} />
+              <Route path="/models/:id/edit" element={<EditModel />} />
+              <Route path="/roadmap" element={<Roadmap />} />
+              <Route path="/restok" element={<ReStok />} />
+            </Route>
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </FontSizeProvider>
+    </SearchProvider> {/* ADD THIS */}
   </QueryClientProvider>
 );
 
